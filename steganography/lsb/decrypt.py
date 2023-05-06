f7dd3cec28b7b179b32f85a21d6cfca09a9916ae
Arpan Pandey <arpan@hackersreboot.tech>
2023-05-06 22:32:14 +0530

    Added some comments

--- steganography/lsb/decrypt.py
+++ steganography/lsb/decrypt.py
@@ -6,7 +6,9 @@ from util.key_gen import key_gen
 from stegano import lsb
 
 def decrypt_image(input_image_path):
-
+    """
+    Decrypts the image
+    """
     decrypted_text = lsb.reveal(input_image_path)
 
     return decrypted_text
